7dc74d5f47727e89eda04c42e5adb8dc53c598c9
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-04-30 19:26:52 +0200

    hw-8/1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,16 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.min.js";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const images = document.querySelectorAll('img');
+const lightbox = new SimpleLightbox('.gallery a');
+
+images.forEach(image => {
+  image.addEventListener('click', (event) => {
+    event.preventDefault();
+    lightbox.open({ src: event.target.src });
+  });
+});
