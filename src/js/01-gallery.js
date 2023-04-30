c9de8643dfbaac1fd87f8431948bb86b5baae396
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-04-30 20:03:58 +0200

    111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,25 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems as items } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+console.log(items);
+
 
 import SimpleLightbox from 'simplelightbox';
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
-galleryEl.innerHTML = markupGalleryItems(galleryItems);
+galleryEl.innerHTML = markupGallery(items);
 
 const gallery = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 250,
+captionsData: 'alt',
+captionDelay: 250,
 });
 
-function markupGalleryItems(listItems) {
-  return listItems
-    .map(
-      item => `
-          <a class="gallery__item" href="${item.original}">
-            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-          </a>
-        `
-    )
-    .join('');
+function markupGallery(listItems) {
+return listItems
+.map(
+item => <a class="gallery__item" href="${item.original}"> <img class="gallery__image" src="${item.preview}" alt="${item.description}" /> </a> )
+.join('');
 }
