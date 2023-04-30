85402b335443975b25ce96d01e07d203b2119418
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-04-30 20:29:50 +0200

    333

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,60 @@
 
+import { throttle } from 'lodash.throttle';
+
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+
+const feedbackFormStateKey = 'feedback-form-state';
+
+
+emailInput.addEventListener(
+'input',
+throttle(() => {
+
+const state = {
+email: emailInput.value,
+message: messageInput.value,
+};
+localStorage.setItem(feedbackFormStateKey, JSON.stringify(state));
+}, 500)
+);
+
+messageInput.addEventListener(
+'input',
+throttle(() => {
+
+const state = {
+email: emailInput.value,
+message: messageInput.value,
+};
+localStorage.setItem(feedbackFormStateKey, JSON.stringify(state));
+}, 500)
+);
+
+
+const savedState = localStorage.getItem(feedbackFormStateKey);
+if (savedState) {
+
+const state = JSON.parse(savedState);
+emailInput.value = state.email;
+messageInput.value = state.message;
+}
+
+
+feedbackForm.addEventListener('submit', (event) => {
+event.preventDefault();
+
+localStorage.removeItem(feedbackFormStateKey);
+
+emailInput.value = '';
+messageInput.value = '';
+
+const state = {
+email: emailInput.value,
+message: messageInput.value,
+};
+console.log(state);
+});
