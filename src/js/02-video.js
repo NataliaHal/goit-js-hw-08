fa8397fe3e910f48946c6105370186b0bcea243a
NataliaHal <118615900+NataliaHal@users.noreply.github.com>
2023-04-30 20:14:46 +0200

    222

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', _.throttle(() => {
+  const currentTime = player.getCurrentTime();
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
 
